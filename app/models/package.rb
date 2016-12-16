08b40f4c9d5a8613dd20ecdbd6158b973b4ad941
djsegal <djsegal@wisc.edu>
2016-12-16 01:54:23 -0500

    Annotate packages

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: packages
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Package < ApplicationRecord
 end
--- test/models/package_test.rb
+++ test/models/package_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: packages
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class PackageTest < ActiveSupport::TestCase
