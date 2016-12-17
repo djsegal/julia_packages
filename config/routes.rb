1e234a32775c97eb4d0801ffff3670186cdc552e
djsegal <djsegal@wisc.edu>
2016-12-17 15:25:39 -0500

    Improve breadcrumb for every action

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :counters
   resources :versions
   resources :repositories
+
   resources :packages do
     get 'autocomplete_package_name', on: :collection
   end
