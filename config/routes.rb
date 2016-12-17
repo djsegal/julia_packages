eb249975b60702276cdf4f1f11286359c8840a9d
djsegal <djsegal@wisc.edu>
2016-12-16 21:25:09 -0500

    Move some lines of code around in the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root 'home#index'
-  get 'home/index'
   resources :daters
   resources :counters
   resources :versions
   resources :repositories
   resources :packages
 
+  root 'home#index'
+  get 'home/index'
 end
