0d295541bfb5b0c3d455e434a54374ba74b250f3
djsegal <djsegal@wisc.edu>
2016-12-18 21:24:34 -0500

    Move category making into its own process rakeTask

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-categories = []
-
-Organization.all.each do |organization|
-  next unless organization.name.include? 'Julia'
-  next if organization.owned_packages.count < 3
-
-  categories << organization.name.gsub('Julia', '')
-end
-
-categories.each do |category|
-  Category.create! name: category
-end
