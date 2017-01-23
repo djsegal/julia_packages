e0bfc4cb25d0868b92bfc1f89fc3ba6b1959c043
Dan Segal <danSegal2@gmail.com>
2017-01-22 21:58:32 -0600

    Correctly catch ActionView Template errors in prod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   if Rails.env.production?
-    rescue_from \
-      StandardError, \
-      with: :error_render_method
+    caught_errors = [
+      ActionView::Template::Error,
+      StandardError
+    ]
+
+    caught_errors.each do |caught_error|
+      rescue_from caught_error, \
+        with: :error_render_method
+    end
   end
 
   def error_render_method
