3d4924744a657b430b14ba8719b9cec98fdeeb46
Dan Segal <danSegal2@gmail.com>
2017-01-23 14:39:47 -0600

    Get emails working in production

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,6 @@ Bundler.require(*Rails.groups)
 module JuliaObserver
   class Application < Rails::Application
 
-    # djs - get mail working
-    config.action_mailer.smtp_settings = { enable_starttls_auto: false  }
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "julia_observer_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = \
+    { host: 'juliaobserver.com' }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = \
+    {
+      port: 25,
+      address: 'localhost',
+      domain: 'juliaobserver.com',
+      enable_starttls_auto: false
+    }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
