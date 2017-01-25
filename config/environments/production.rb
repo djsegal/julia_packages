85e9b1065d6380c63ad3f0f35c62fef7a187da75
Dan Segal <danSegal2@gmail.com>
2017-01-24 18:13:56 -0600

    Fix host problem in production (i.e. cdn.blah.com)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_controller.default_url_options = \
+    { host: 'juliaobserver.com' }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
