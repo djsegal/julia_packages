f6d3fcd42bc5f2769ab5da337688dc95777e3267
Dan Segal <danSegal2@gmail.com>
2017-02-13 16:56:48 -0500

    Make news querying happen as late as possible

--- app/jobs/download_job.rb
+++ app/jobs/download_job.rb
@@ -5,9 +5,6 @@ class DownloadJob < JuliaJob
     Batch.current_marker_date = Time.zone.now
     set_batch_marker :current_date, Batch.current_marker_date
 
-    FileUtils.rm_rf "tmp/news"
-    system "#{@sys_run} news:get_all"
-
     FileUtils.rm_rf "tmp/github"
     system "#{@sys_run} github:download"
   end
