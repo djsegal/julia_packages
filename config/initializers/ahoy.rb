90fa32205edbbfcd630ab3de16e3fa4d1497143b
Dan Segal <danSegal2@gmail.com>
2017-06-25 13:37:18 -0400

    Configure Ahoy to try to collect more data

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,3 +1,9 @@
 class Ahoy::Store < Ahoy::Stores::ActiveRecordTokenStore
-  # customize here
+
 end
+
+Ahoy.geocode = :async
+Ahoy.track_visits_immediately = true
+Ahoy.cookie_domain = :all
+Ahoy.throttle_limit = 100
+Ahoy.throttle_period = 5.minutes
