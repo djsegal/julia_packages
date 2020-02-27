cdf9bb29baeb81426c34abb93b5412a82b0175e8
Dan Segal <dansegal2@gmail.com>
2020-02-26 22:59:01 -0800

    Make it so scour just purges week old (bad) ones

--- app/jobs/scour_job.rb
+++ app/jobs/scour_job.rb
@@ -2,7 +2,16 @@ class ScourJob < JuliaJob
   queue_as :default
 
   def perform(*args)
-    FileUtils.rm_rf "tmp/scour"
+    Dir.foreach("tmp/scour") do |directory|
+      next if directory.starts_with? '.'
+      file_name = "tmp/scour/#{directory}/data.yml"
+
+      next unless File.exist? file_name
+      updated_time = File.mtime file_name
+
+      next if updated_time > 1.week.ago
+      FileUtils.rm_rf "tmp/scour/#{directory}"
+    end
 
     system "#{@sys_run} scour:packages"
   end
