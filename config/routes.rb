cff234d0be5d440fbe9b122208f7e1beedf51d61
Dan Segal <dansegal2@gmail.com>
2020-03-24 01:13:45 -0700

    Make packages#index route the root route of app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :packages
+
+  root to: 'packages#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
