9b08a53b4d599c748c86a6dd76286dd0f7a22053
Dan Segal <dansegal2@gmail.com>
2020-03-28 04:25:50 -0700

    Add category and subcategory constructor to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -58,5 +58,40 @@ end
 Readme.destroy_all
 Readme.import readme_list, batch_size: 512
 
+Category.destroy_all
+
 trending_packages = Package.where(name: trending_db["package"].to_a)
 Category.create! name: "Trending", packages: trending_packages
+
+category_names = $category_tree.keys
+sub_category_names = $category_tree.values.flatten
+
+category_package_names_list = category_names.map {
+  |cur_name| decibans_db.filter(:row) do |cur_row|
+    cur_row["category"] == cur_name
+  end
+}.map(&:package).map(&:to_a)
+
+sub_category_package_names_list = sub_category_names.map {
+  |cur_name| decibans_db.filter(:row) do |cur_row|
+    cur_row["sub_category"] == cur_name
+  end
+}.map(&:package).map(&:to_a)
+
+category_names.zip(category_package_names_list) do |category_name, category_package_names|
+  category_packages = Package.where(name: category_package_names)
+  next unless category_packages.length > 4
+
+  Category.create! \
+    name: category_name, \
+    packages: category_packages
+end
+
+sub_category_names.zip(sub_category_package_names_list) do |sub_category_name, sub_category_packages_names|
+  sub_category_packages = Package.where(name: sub_category_packages_names)
+  next unless sub_category_packages.length > 4
+
+  Category.create! \
+    name: sub_category_name, \
+    packages: sub_category_packages
+end
