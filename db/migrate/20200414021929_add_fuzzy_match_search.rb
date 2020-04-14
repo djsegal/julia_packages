a6bfaae1af88c73431cbe22654c1572226564bf1
Dan Segal <dansegal2@gmail.com>
2020-04-13 19:44:41 -0700

    Add trigram and dmetaphone search for postgresql

---
+++ db/migrate/20200414021929_add_fuzzy_match_search.rb
@@ -0,0 +1,11 @@
+class AddFuzzyMatchSearch < ActiveRecord::Migration[6.0]
+  def self.up
+    ActiveRecord::Base.connection.execute("CREATE EXTENSION IF NOT EXISTS pg_trgm;")
+    ActiveRecord::Base.connection.execute("CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;")
+  end
+
+  def self.down
+    ActiveRecord::Base.connection.execute("DROP EXTENSION IF EXISTS pg_trgm;")
+    ActiveRecord::Base.connection.execute("DROP EXTENSION IF EXISTS fuzzystrmatch;")
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_09_113123) do
+ActiveRecord::Schema.define(version: 2020_04_14_021947) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "fuzzystrmatch"
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   create_table "ahoy_events", force: :cascade do |t|
