aa08ef20022323d7f6d4fb919908e8b528036b34
Dan Segal <dansegal2@gmail.com>
2020-04-13 20:06:25 -0700

    Remove unneeded postgres search libraries

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_021947) do
+ActiveRecord::Schema.define(version: 2020_04_14_030345) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "fuzzystrmatch"
-  enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   create_table "ahoy_events", force: :cascade do |t|
