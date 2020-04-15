3eb8d18e3114891bc8133f9fa88a686ae0a6cbbb
Dan Segal <dansegal2@gmail.com>
2020-04-14 23:09:51 -0700

    Add total counts to page views and users on analytics page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,21 @@ module ApplicationHelper
     return ' active' if request.path == cur_path
     ''
   end
+
+  def custom_int_to_text(cur_int)
+    return 0 if cur_int.zero?
+
+    cur_round = 2 - Math.log10(cur_int).ceil
+    rounded_int = cur_int.round(cur_round)
+
+    used_units = {
+      thousand: "k", million: "m", billion: "b"
+    }
+
+    cur_text = number_to_human(
+      rounded_int, format: '%n%u', units: used_units
+    )
+
+    cur_text
+  end
 end
