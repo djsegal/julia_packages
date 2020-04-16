660f6f015b9afbc6b7634b905444b6efa021e897
Dan Segal <dansegal2@gmail.com>
2020-04-15 18:29:36 -0700

    Add link badges to show package page for user and associated category

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -80,7 +80,7 @@ class PackagesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_package
-      @package = Package.friendly.find(params[:id].downcase.gsub(".jl", ""))
+      @package = Package.includes(:categories).friendly.find(params[:id].downcase.gsub(".jl", ""))
     end
 
     # Only allow a list of trusted parameters through.
